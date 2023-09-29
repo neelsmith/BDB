c33b9c117126137b3543c6e107c719a786912cbc
Neel Smith <dnsmith.neel@gmail.com>
2023-09-29 10:23:09 -0400

    Identify lemmata

--- scripts/lemmata.jl
+++ scripts/lemmata.jl
@@ -7,21 +7,36 @@ f = joinpath(pwd(), "src", "lex-wellformed.cex")
 # Skip header:
 lines = readlines(f)[2:end]
 
-lemma_re = r"\|([IV., ]*)<bdbheb>([^<]+)</bdbheb>[, ]*"
+hebrew_lemma = r"\|([IV., ]*)<bdbheb>([^<]+)</bdbheb>[, ]*"
 
 
-function missinglemma(rawlines)
+function missinglemma(rawlines, lemma_re)
     nolemma = []
-    for (i, ln) in enumerate(rawlines)
-        lineno = i + 1
-        
+    for (i, ln) in enumerate(rawlines)        
         if occursin(lemma_re, ln)
         else
-            @warn("Failed to match on line $(lineno)")
-            push!(nolemma, lineno)
+            #@warn("Failed to match on line $(i)")
+            push!(nolemma, i)
         end
     end
     nolemma
 end
 
-missing = missinglemma(lines)
+missing = missinglemma(lines, hebrew_lemma)
+
+@info("$(length(missing)) articles did not have a recognizable Hebrew lemma")
+
+aramaic_lemma= r"\|([\[IV., ]*)<bdbarc>([^<]+)</bdbarc>[, ]*"
+nolemma = []
+for i in missing
+    @info(string(i,". ", lines[i]))
+    if occursin(aramaic_lemma, lines[i])
+    else
+
+        #@warn("Failed to match on line $(i)")
+        push!(nolemma, i)
+    end
+end
+
+nolemma |> println
+@info("For $(length(nolemma)) entries, failed to recognize either lemma.")
